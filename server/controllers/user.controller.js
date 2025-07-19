af8a8431465cf4d637660c0c29ab3a42f9d755be
Ariella Rollins <berryrue@gmail.com>
2025-07-18 21:25:47 -0700

    added more routes

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -24,15 +24,15 @@ export const loginUser = async (req, res) => {
                 username: data.username,
             },
         });
-
     }
     else { res.status(401).json("User password or email is not valid.") }
 }
 
-// after logging in, get's user info
-export const getCurrentUser = async (req, res) => {
+// checks if username exists in db
+export const checkUserName = async (req, res) => {
     try {
-        const USER = await User.findById(req.user._id).select(`-password`)
+        console.log("req", req.query)
+        const USER = await User.findOne({ username: req.query });
         if (!USER) {
             return res.status(404).json({ message: 'User not found.' })
         }
@@ -42,19 +42,10 @@ export const getCurrentUser = async (req, res) => {
     }
 }
 
-// for getting family. front-end will filter out.
-export const getAllUsers = async (req, res) => {
-    try {
-        const USERS = await User.find().select(`-password`)
-        res.status(200).json(USERS)
-    } catch (error) { res.status(400).json(error) }
-}
-
-
 export const registerUser = async (req, res) => {
     console.log("entered register controller. req.body:", req.body)
     try {
-        const user = await User.create(req.body)
+        const user = await User.create({...req.body, children: [], parents: [], choresCompleted:[], isVerified: false, isActive: true })
         delete user.password //removes password from the object
         const token = generateToken(user._id);
         const data = {
@@ -78,6 +69,26 @@ export const registerUser = async (req, res) => {
     }
 }
 
+// after logging in, get's user info
+export const getCurrentUser = async (req, res) => {
+    try {
+        const USER = await User.findById(req.user._id).select(`-password`)
+        if (!USER) {
+            return res.status(404).json({ message: 'User not found.' })
+        }
+        res.status(200).json(USER)
+    } catch (error) {
+        res.status(400).json({ message: error.message || 'An error occurred while fetching the profile.' })
+    }
+}
+
+// for getting family. front-end will filter out.
+export const getAllUsers = async (req, res) => {
+    try {
+        const USERS = await User.find().select(`-password`)
+        res.status(200).json(USERS)
+    } catch (error) { res.status(400).json(error) }
+}
 
 
 // Use for "deleting users". Frontend changes user attribute active => inactive
