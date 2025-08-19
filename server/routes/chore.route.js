3d31f21a1dd354ab4b329ab8d9b7105cabb237d0
Ariella Rollins <berryrue@gmail.com>
2025-08-19 13:50:22 -0700

    added chore controllers

--- server/routes/chore.route.js
+++ server/routes/chore.route.js
@@ -1,20 +1,28 @@
 import { Router } from "express"
 import { protect } from "../middleware/authMiddleware.js"
-import { addChore, getAllChores, getChoresByWorker, deleteAllChores, updateChore, deleteChore, getTemplates } from "../controllers/chore.controller.js"
+import { addChore, getAllChores, getChoreById, getChoresByWorker, deleteAllChores, updateChore, deleteChore, getTemplates, getChoresByParent } from "../controllers/chore.controller.js"
 
 const choreRouter = Router()
 
 
 choreRouter.route('/all')
-    .get( getAllChores )
-    .delete ( deleteAllChores)
+    .get(getAllChores)
+    .delete(deleteAllChores)
 
 choreRouter.route('/')
-// add protect after done testing
+    // add protect after done testing
     .post(addChore)
-    .get(getChoresByWorker)
+    .get(getChoreById)
     .put(protect, updateChore)
-    .delete (deleteChore)
+    .delete(deleteChore)
+
+    
+choreRouter.route('/worker')
+    .get(getChoresByWorker)
+
+choreRouter.route('/parents')
+    .get(getChoresByParent)
+
 
 choreRouter.route('/templates')
     .get(getTemplates)
